5e0afca151988ed56ba091ceba04e006e08bea58
Rifat Dzhumagulov <rdzhumagulov@samokat.ru>
2024-01-30 13:06:29 +0300

    Исправление шежире

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import "./styles.css";
 
 import App from "./App";
 
-const rootEl = document.getElementById("root");
-if(rootEl) {
-    const root = createRoot(rootEl);
-    root.render(
-        <StrictMode>
-            <App/>
-        </StrictMode>
-    );
-}
+setTimeout(()=>{
+    const rootEl = document.getElementById("root");
+    if(rootEl) {
+        const root = createRoot(rootEl);
+        root.render(
+            <StrictMode>
+                <App/>
+            </StrictMode>
+        );
+    }
+}, 1e3);
